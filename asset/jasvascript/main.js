307d4a5b8f2b3451ecd32d16afa840e49bb49d35
AnicetChiza <chizaanicet@gmail.com>
2024-09-25 01:40:05 +0200

    add to my repository

--- asset/jasvascript/main.js
+++ asset/jasvascript/main.js
@@ -41,3 +41,46 @@ document.addEventListener('DOMContentLoaded', () => {
     // Auto-scroll functionality
     setInterval(showNextItem, 5000);
 });
+
+/*----------------------------------
+#Menu
+----------------------------------*/
+document.addEventListener('DOMContentLoaded', () => {
+    const list = document.querySelector('.header-list ul');
+    const exit = document.querySelector('.exit');
+    const menuIcon = document.querySelector('.menu-icon');
+    const overlay = document.querySelector('.overlay');
+    const menuItems = document.querySelectorAll('.header-list ul li a');
+
+    // if (!list) console.error('List element not found');
+    // if (!exit) console.error('Exit element not found');
+    // if (!menuIcon) console.error('Menu icon element not found');
+    // if (!overlay) console.error('Overlay element not found');
+
+    if (menuIcon) {
+        menuIcon.addEventListener('click', () => {
+            list.classList.add('active');
+            overlay.classList.add('active');
+            menuIcon.style.display = 'none';
+            exit.style.display = 'flex';
+        });
+    }
+
+    if (exit) {
+        exit.addEventListener('click', () => {
+            list.classList.remove('active');
+            overlay.classList.remove('active');
+            exit.style.display = 'none';
+            menuIcon.style.display = 'block';
+        });
+    }
+
+    menuItems.forEach((item) => {
+        item.addEventListener('click', () => {
+            list.classList.remove('active');
+            overlay.classList.remove('active');
+            exit.style.display = 'none';
+            menuIcon.style.display = 'block';
+        });
+    });
+});
