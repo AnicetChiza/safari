2bbe585c1cc70aad0515a0f262aca6eb67d682ab
AnicetChiza <chizaanicet@gmail.com>
2024-09-25 10:02:15 +0200

    add to my repository

--- asset/jasvascript/main.js
+++ asset/jasvascript/main.js
@@ -84,3 +84,18 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
+/*------------------------------
+#For the header
+/-----------------------------*/
+jQuery(function ($) {
+    const $header = $('.header');
+    $(window).scroll(function (event) {
+        const $current = $(this).scrollTop();
+        if ($current > 160) {
+            $header.addClass('header-color');
+        } else {
+            $header.removeClass('header-color');
+        }
+    });
+});
